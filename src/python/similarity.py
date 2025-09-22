f4a80a1e91f11fe9d60a883840217a5f510c8510
lamurian <lamuri.aly8@gmail.com>
2025-09-22 07:56:49 +0200

    Measure semantic similarity

--- src/python/similarity.py
+++ src/python/similarity.py
@@ -24,3 +24,36 @@ tbl_sim = pd.DataFrame(sims)
 
 # Write a csv file
 tbl_sim.to_csv("data/processed/coding-similarity.csv", sep = ",", index = False)
+
+# Set MC and HC interpretation values
+MC = [
+    "Stress adversely affects mental health through emotional responses, leading to maladaptive coping strategies like substance use and social media engagement.",
+    "Resilience acts as a protective buffer against stress, enabling active coping strategies and adaptation through personal attributes, ultimately fostering psychological well-being amid challenges."
+]
+
+HC1 = [
+    "Stressful life events decompensate psychological resources and cause a long-lasting mental health impact, which in turn incite reward-seeking behavior",
+    "An adequate resource is necessary to circumvent stressful life event and develop resilience"
+]
+
+HC2 = "Resilience plays a crucial role in mental health dynamics by acting as an adaptive function and coping mechanism that moderates the long-lasting impact of stress-induced strain and reward-seeking behavior, facilitating self-regulation, resource management, and the process of overcoming stress to enhance well-being and contribute to successful aging."
+
+# Measure similarity between the MC and HC1 interpretation
+for interpretation in zip(MC, HC1):
+    embed = model.encode(interpretation, convert_to_tensor = True)
+    sim = util.cos_sim(embed, embed)
+    print(sim)
+
+embed = model.encode(["".join(MC), "".join(HC1)])
+sim = util.cos_sim(embed, embed)
+print(sim)
+
+# Measure similarity between the MC and HC2 interpretation
+embed = model.encode(["".join(MC), HC2])
+sim = util.cos_sim(embed, embed)
+print(sim)
+
+# Measure similarity between the HC1 and HC2 interpretation
+embed = model.encode(["".join(HC1), HC2])
+sim = util.cos_sim(embed, embed)
+print(sim)
